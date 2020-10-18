4f5cd56c6eb0b75c88032a85bfb919b1e0edd063
YouTubePlays <allplayslive@gmail.com>
2020-10-17 20:56:56 -0700

    feat:Faq initial

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,9 +1,50 @@
 import React from 'react';
 import { withNamespaces } from 'react-i18next';
+import { makeStyles } from '@material-ui/core/styles';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+    },
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
+        fontWeight: theme.typography.fontWeightBold,
+    },
+    body: {
+        fontSize: theme.typography.pxToRem(15),
+        fontWeight: theme.typography.fontWeightRegular,
+    },
+}));
 
 
 
 function Faq(props) {
-    return (<div></div>);
+    const { t } = props;
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            {t('faq.questions', { returnObjects: true }).map((question, index) => (
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                        <Typography className={classes.heading}>{question}</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography className={classes.body}>
+                            <div dangerouslySetInnerHTML={{ __html: t('faq.answers', { returnObjects: true })[index]}} />
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+            ))}
+        </div>
+    );
 }
 export default withNamespaces()(Faq);
